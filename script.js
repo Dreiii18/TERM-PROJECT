f22d3631d5e9767084dc3e0cd06eb2868928ca10
ANDREI ARTAP <andreiartap@gmail.com>
2023-11-11 00:13:49 -0800

    Co-authored-by: branden <brndnh@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+const button = document.getElementById('reset');
 
 const CANVAS_HEIGHT = canvas.height;
 const CANVAS_WIDTH = canvas.width;
 const GROUND_HEIGHT = 200;
 const GROUND_LINE = CANVAS_HEIGHT - GROUND_HEIGHT;
 const CUBE_SIZE = 50;
-let element;
 
-//JUMP MOTION
-const JUMP_PACE = 5;
+//--JUMP MOTION--
+const JUMP_PACE = 3;
 let jump = 0;
-let jump_height = 6;
-let jump_timer;
-let rotationAngle = 0;
 let movingDown = false;
+let jump_timer;
+let jump_timer_active = false;
 
-//OBSTACLE MOTION
-const obstacle = document.createElement("canvas");
-const obstacleCanvas = obstacle.getContext("2d");
-obstacleCanvas.width = CANVAS_WIDTH;
-obstacleCanvas.height = CANVAS_HEIGHT;
+//--OBSTACLE MOTION--
 const OBSTACLE_PACE = 10;
-let moveHorizontal = 0;
-let obstacle_timer;
-let moveLeft = false;
 let obstacle_position = CANVAS_WIDTH + CUBE_SIZE;
+let moveHorizontal = 0;
+let moveLeft = false;
+let obstacle_timer_active = false;
 
-//if player status = 0, game is over
-let playerStatus = 1;
+//--IF GAME STATUS = 0, GAME IS OVER GAME RESETS--
+let gameStatus = 1;
 
-// colours for background and ground
-let r = 0, g = 0, b = 200;
-
-// for music
+//--FOR MUSIC--
 const audioPlayer = new Audio('BaseAfterBase.mp3');
 
+//--SCROLLING GROUND VARIABLES--
+const squarePace = 2;
+let squarePosition = 0;
+let r = 0, g = 0, b = 200;
+
+//--GAME OVER TEXT--
+const centerX = canvas.width / 2
+const centerY = canvas.height / 2
+
+//-----DRAW FUNCTIONS-----
 function drawBackground() {
     ctx.save();
-    ctx.fillStyle = "rgb(" + r + ", " + g + ", " + b + ")";
+    const gradient = ctx.createLinearGradient(0, 0, 0, CANVAS_HEIGHT/2);
+    gradient.addColorStop(0, "black");
+    gradient.addColorStop(1, "rgb(" + (r - 75) + ", " + (g - 75) + ", " + (b - 75) + ")");
+
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT - GROUND_HEIGHT);
+    ctx.restore();
+}
+
+function drawGround() {
+    ctx.save();
+    ctx.fillStyle = "rgb("+r +", "+g+", "+b+")";
     ctx.translate(0, GROUND_LINE);
     ctx.beginPath();
     ctx.lineTo(0, 0);
@@ -47,13 +60,7 @@ function drawBackground() {
     ctx.lineTo(0, GROUND_HEIGHT);
     ctx.fill();
     ctx.restore();
-
-    const squareWidth = (CANVAS_WIDTH - 9 * 25) / 10;
-    ctx.fillStyle = "rgb(" + (r - 50) + ", " + (g - 50) + ", " + (b - 50) + ")";
-    for (let i = 0; i < 12; i++) {
-        const x = i * (squareWidth + 25);
-        ctx.fillRect(x, GROUND_LINE + 25, squareWidth, GROUND_HEIGHT - 25);
-    }
+    drawScrollingGround();
 }
 
 function drawBox() {
@@ -61,14 +68,34 @@ function drawBox() {
     ctx.fillRect(CUBE_SIZE + 25, GROUND_LINE - CUBE_SIZE,CUBE_SIZE,CUBE_SIZE);
 }
 
+function drawObstacles() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(obstacle_position, GROUND_LINE - CUBE_SIZE,CUBE_SIZE,CUBE_SIZE);
+}
+
+function drawScrollingGround() {
+    const squareWidth = CANVAS_WIDTH / 10;
+    ctx.fillStyle = "rgb("+(r - 50)+", "+(g - 50)+", "+(b - 50)+")";
+    
+    for (let i = 0; i < 10; i++) {
+        let x = (i * (squareWidth + 25) + squarePosition);
+        ctx.fillRect(x, GROUND_LINE + 25, squareWidth, GROUND_HEIGHT - 25);
+    }
+    squarePosition -= squarePace;
+
+    if (squarePosition <= -squareWidth - 25) {
+        squarePosition = 0;
+    }
+}
+
+//-----MOVE FUNCTIONS-----
 function boxJump() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     drawBackground();
+    drawObstacles();
+    
     ctx.save();
-    console.log(jump);
     if (movingDown === false) {
         jump--;
-        rotationAngle += 6;
         if (jump === -(CUBE_SIZE * 2)) {
             movingDown = true;
         }
@@ -77,6 +104,7 @@ function boxJump() {
         if (jump === 0) {
             movingDown = false;
             clearInterval(jump_timer);
+            jump_timer_active = false;
         }
     }
     ctx.translate(0, jump);
@@ -84,53 +112,113 @@ function boxJump() {
     ctx.restore();
 }
 
-function drawObstacles() {
-    ctx.fillStyle = 'red';
-    // ctx.fillRect(CUBE_SIZE + CANVAS_WIDTH, GROUND_LINE - CUBE_SIZE,CUBE_SIZE,CUBE_SIZE);
-    ctx.fillRect(obstacle_position, GROUND_LINE - CUBE_SIZE,CUBE_SIZE,CUBE_SIZE);
-}
-
 function moveObstacles() {
-    // ctx.save();
-    // moveHorizontal--;
-    // if (moveHorizontal <= -CANVAS_WIDTH) {
-    //     moveHorizontal = 0;
-    // }
-    
-    // ctx.translate(moveHorizontal, 0);
-    // drawObstacles();
-    // ctx.restore();
     if (jump === 0){
-        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        drawBox();
         drawBackground();
+        drawBox();
     } else {
-        ctx.clearRect(obstacle_position - JUMP_PACE, GROUND_LINE - CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
+        ctx.clearRect(obstacle_position - OBSTACLE_PACE, GROUND_LINE - CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
     }
-    obstacle_position -= JUMP_PACE;
-    console.log(obstacle_position);
+
+    drawGround();
+    drawObstacles();
+    obstacle_position -= OBSTACLE_PACE - 4;
+
     if (obstacle_position <= 0) {
         obstacle_position = CANVAS_WIDTH + CUBE_SIZE;
     }
-    drawObstacles();
+    collision();
 }
 
+//-----COLLISION VALIDATION-----
+function collision() {
+    let CUBE_X = CUBE_SIZE + 25;
+    let CUBE_Y = GROUND_LINE - CUBE_SIZE - jump;
+    let OBSTACLE_X = obstacle_position;
+    let OBSTACLE_Y = GROUND_LINE - CUBE_SIZE;
+
+    if (CUBE_X < OBSTACLE_X + CUBE_SIZE &&
+        CUBE_X + CUBE_SIZE > OBSTACLE_X &&
+        CUBE_Y < OBSTACLE_Y + CUBE_SIZE &&
+        CUBE_Y + CUBE_SIZE > OBSTACLE_Y
+        ) {
+            audioPlayer.pause();
+            stopTimers();
+        }
+}
+
+//-----TIMERS-----
 function startTimers() {
-    if (jump === 0) {
+    if (!jump_timer_active && jump === 0) {
+        jump_timer_active = true;
         jump_timer = setInterval(boxJump, JUMP_PACE);
-        obstacle_timer = setInterval(moveObstacles, JUMP_PACE);
+    }
+    
+    if (!obstacle_timer_active) {
+        obstacle_timer_active = true;
+        obstacle_timer = setInterval(moveObstacles, OBSTACLE_PACE);
     }
 }
 
-function init() {
+function stopTimers() {
+    gameStatus = 0;
+    clearInterval(jump_timer);
+    clearInterval(obstacle_timer);
+    obstacle_timer_active = false;
+    jump_timer_active = false;
+    gameOverText();
+}
+
+function gameOverText() {
+    ctx.textAlign = "center";
+    ctx.fillStyle = "red";
+    ctx.font = "125px Comic Sans MS";
+    ctx.fillText("GAME OVER!", centerX + 2, centerY + 2);
+    ctx.font = "40px Comic Sans MS";
+    ctx.fillText("You crashed.", centerX + 2, centerY + 52);
+
+    ctx.font = "125px Comic Sans MS";
+    ctx.fillText("GAME OVER!", centerX - 2, centerY - 2);
+    ctx.font = "40px Comic Sans MS";
+    ctx.fillText("You crashed.", centerX - 2, centerY + 48);
+
+    ctx.fillStyle = "white";
+    ctx.font = "125px Comic Sans MS";
+    ctx.fillText("GAME OVER!", centerX, centerY);
+    ctx.font = "40px Comic Sans MS";
+    ctx.fillText("You crashed.", centerX, centerY + 50);
+    button.style.display = 'flex';
+  }
+
+function resetButton() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    obstacle_position = -CANVAS_WIDTH;
+    jump = 0;
+    movingDown = false;
     drawBackground();
+    drawGround();
+    drawBox();
+    gameStatus = 1;
+    button.style.display = 'none';
+    audioPlayer.currentTime = 0;
+}
+
+
+function init() {
+    gameStatus = 1
+    drawBackground();
+    drawGround();
     drawBox();
     document.addEventListener('keyup', function(event){
-        audioPlayer.play();
-        if ((event.key === " ") && (jump === 0)) {
-            startTimers();
+        if (gameStatus === 1) {
+            if (((event.key === " ") && (jump === 0)) || ((event.key === "ArrowUp") && (jump === 0))) {
+                audioPlayer.play();
+                startTimers();
+            } 
+        } else {
+            gameStatus = 0;
         }
-    })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
